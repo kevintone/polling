8852be3f5a2ef9eebacf156cd4c819d677a0f5ee
kevintone <kevinton@hawaii.edu>
2015-09-30 11:50:01 -1000

    added subscribe/publish to javascript

--- collections/polls.js
+++ collections/polls.js
@@ -1,3 +1,14 @@
 
 
 Polls = new Mongo.Collection('polls');
+
+if (Meteor.isServer) {
+    Meteor.publish('polls', function(){
+        return Polls.find();
+    });
+}
+
+if (Meteor.isClient) {
+    Meteor.subscribe('polls');
+
+}
